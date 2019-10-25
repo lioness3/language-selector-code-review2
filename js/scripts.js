b2433f6118f88ccf8d11cfa5bc6c386be503f14b
Joann Carter <jcnailtech3@yahoo.com>
2019-10-25 12:01:54 -0700

    add variables and if else

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,28 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-$(".btn").submit(language);
+$(".quiz").submit(function(event){
+
+var light = $("#light:checked").val();
+var noLight = $("#noLight:checked").val();
+var sunglasses = $("#sunglasses:checked").val();
+var orange = $("#orange1:checked").val();
+var purple = $("#purple2:checked").val();
+var green = $("#green3:checked").val();
+
+var alien = $("#alien:checked").val();
 
 
-var 4 = .question4
+var coffee = $("#coffee:checked").val();
 
+var office = $("#office:checked").val();
 
+if(light && purple && alien && coffee && office ){
+prompt("JAVASCRIPT");
 
+}else {
+  alert("NO!")
+}
 
-})
+event.preventDefault();
+});
+});
