e158930643f00469544dccd9f1f90ac4855e5dc0
Joann Carter <jcnailtech3@yahoo.com>
2019-10-25 12:33:16 -0700

    add more variables and switch script error

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,34 @@
 $(document).ready(function() {
 
 $(".quiz").submit(function(event){
-
+//question1
 var light = $("#light:checked").val();
 var noLight = $("#noLight:checked").val();
 var sunglasses = $("#sunglasses:checked").val();
+//question2
 var orange = $("#orange1:checked").val();
 var purple = $("#purple2:checked").val();
 var green = $("#green3:checked").val();
-
+//question3
 var alien = $("#alien:checked").val();
-
-
+var money = $("#money:checked").val();
+var puzzle = $("#puzzle;checked").val();
+//question4
+var martini = $("#martini:checked").val();
 var coffee = $("#coffee:checked").val();
-
+var tea = $("#tea:checked").val();
+//question5
 var office = $("#office:checked").val();
+var jokers = $("#jokers:checked").val();
+var thrones = $("#thrones:checked").val();
+
+
 
 if(light && purple && alien && coffee && office ){
-prompt("JAVASCRIPT");
+alert("JAVASCRIPT");
 
 }else {
-  alert("NO!")
+  alert("NO!");
 }
 
 event.preventDefault();
